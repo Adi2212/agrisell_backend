2a1f6168c6403ae618c2d004076fe75bb0e887b1
Adi patil <117048845+Adi2212@users.noreply.github.com>
2026-01-02 11:47:36 +0530

    Update allowed origins for CORS configuration

--- src/main/java/com/agrisell/security/GlobalCorsConfig.java
+++ src/main/java/com/agrisell/security/GlobalCorsConfig.java
@@ -13,7 +13,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                       .allowedOrigins("http://localhost:5173")
+                       .allowedOrigins("http://localhost:5173","https://agrisell-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
